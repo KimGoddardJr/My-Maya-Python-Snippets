46b5a22cae4697d4febb904bc46cf59913386d96
unknown <patxi.aguirre@zhdk.ch>
2021-06-03 13:35:09 +0200

    group and stuff

--- placebonesonrivets.py
+++ placebonesonrivets.py
@@ -1,6 +1,8 @@
 import maya.cmds as cmds
 
 rivet_group = cmds.ls('rivets', dag=1)
+cmds.group( em=True,n='eyelash_joints' )
+
 print(rivet_group)
 
 
@@ -9,8 +11,13 @@ for locator in rivet_group:
         print(locator)
         cmds.select(locator)
         locator_pos = cmds.xform(locator,q=1,ws=1,rp=1)
+        cmds.select(clear=True)
         new_joint = cmds.joint(p=(locator_pos[0],locator_pos[1],locator_pos[2]))
-        print(cmds.xform(locator,q=1,ws=1,rp=1))
+        cmds.select(new_joint)
+        cmds.parent( new_joint, 'eyelash_joints' )
+        cmds.select(clear=True)
+        cmds.parentConstraint(locator,new_joint , maintainOffset=True)
+        
      
         #new_joint = cmds.joint(p=(0,0,0))
         #cmds.select(locator)
